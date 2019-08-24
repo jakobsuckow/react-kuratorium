d96d1ab1579b5ad1637093aba06e0b224661a9e2
jakobsuckow <jakob@martian.agency>
2019-08-24 23:08:26 +0200

    added images

--- src/Components/Releases.js
+++ src/Components/Releases.js
@@ -4,10 +4,22 @@ import React from 'react'
 
 const Releases = () => {
     return (
-        <div>
+        <>
             <h1>Projects</h1>
-            <p>asdasdasdasdasdasdasdsdasda</p>
-        </div>
+            <p></p>
+            <div class="release">
+                <p>Schleifen001</p>
+                <img src="assets/schleifen_release1.jpg" alt="release 1" />
+            </div>
+            <div class="release">
+                <p>Schleifen002</p>
+                <img src="assets/schleifen_release2.jpg" alt="release 3" />
+            </div>
+            <div class="release">
+                <p>Schleifen003</p>
+                <img src="assets/schleifen_release3.jpg" alt="release 2" />
+            </div>
+        </>
     )
 }
 
