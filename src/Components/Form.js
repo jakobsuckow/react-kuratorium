eef7fefb2167fdf8a0c13cf960595315d3d7d5df
jakob@martian.agency <jakob@martian.agency>
2019-08-25 10:06:48 +0200

    added form handler

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import useForm from 'react-hook-form';
 
-const Form = () => {
-    const onSubmit = data => console.log(data)
+
+export default function Form() {
+    const {register, handleSubmit, errors} = useForm();
+    const onSubmit = data => {console.log(data)}
+
     return (
-        <form onSubmit={onSubmit}>
-            <input name="email" />
-            <input type="submit" />
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <label for="email">Email Address:</label>
+        <input name="email" ref={register({
+            required: true,
+            pattern: /^\S+@\S+$/i
+            })}/>
+        {errors.email && 'Email address wrong.'}
+        <input type="submit" name="submit" />
+
         </form>
     )
 }
-
-export default Form
