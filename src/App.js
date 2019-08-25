512213db60a9ed19c88a75c53ed7eb066f315c9e
jakobsuckow <jakob@martian.agency>
2019-08-25 13:41:44 +0200

    started card integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 // Import Slides
 import Dates from './Components/Dates';
@@ -7,13 +7,19 @@ import Releases from './Components/Releases'
 import About from './Components/About'
 
 function App() {
+  const [state, setState] = useState(true);
+
+  function toggle() {
+    state ? setState(false) : setState(true);
+  }
   return (
-    <div className="row">
+    <div className={`${state ? 'row' : 'row__shifted' }`}>
       <div className="intro">
         <Intro />
       </div>
       <div className="about">
         <About />
+        <button onClick={toggle}>Open Card</button>
       </div>
       <div className="releases">
         <Releases />
@@ -21,6 +27,9 @@ function App() {
       <div className="dates">
         <Dates />
 
+      </div>
+      <div className="card">
+        <h1>hi</h1>
       </div>
     </div>
   );
