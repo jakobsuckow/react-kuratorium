7ce18c48a0d67716703efb4114990217a8dcd355
jakobsuckow <jakob@martian.agency>
2019-10-15 17:23:49 +0200

    introducing lambda

---
+++ functions/getFeed.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+require('dotenv').config()
+
+exports.handler = function (event, context, callback) {
+    const apiKey = process.env.AIRTABLE_API_KEY
+    const url = `https://api.airtable.com/v0/appemKlsSSYmto60q/Feed?api_key=${apiKey}`
+
+    // Send Response 
+
+    const send = body => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        })
+    }
+    // Perform API Call
+    const getFeed = () => {
+        axios.get(url)
+        .then(res => send(res.data))
+        .catch(err => send(err))
+    }
+
+    // Make sure Method is "GET"
+    if(event.httpMethod == 'GET') {
+        getFeed()
+    }
+}
