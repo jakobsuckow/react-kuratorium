8373fc366bd96288c164aab1afebe229668334bc
jakobsuckow <jakob@martian.agency>
2019-10-31 11:37:53 +0100

    prettier

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,87 +1,81 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from "react";
+require("dotenv").config();
 const Dates = () => {
+  const feedUrl = `https://cadillac33.netlify.com/.netlify/functions/getFeed`;
+  const datesUrl = `https://cadillac33.netlify.com/.netlify/functions/getDates`;
+  const [dates, setDates] = useState([]);
+  const [feed, setFeed] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const feedUrl = `https://cadillac33.netlify.com/.netlify/functions/getFeed`
-    const datesUrl = `https://cadillac33.netlify.com/.netlify/functions/getDates`
-    const [dates, setDates] = useState([]);
-    const [feed, setFeed] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    getDates();
+    getFeed();
+  }, []);
 
-    useEffect(() => {
-        getDates();
-        getFeed();
-    }, [])
-
-    const getFeed = async() => {
-        setIsLoading(true);
-        const response = await fetch(feedUrl)
-        const data = await response.json();
-        setFeed(data.records)
-        setIsLoading(false)
-
-    }
-    const getDates = async() => {
-        setIsLoading(true);
-        const response = await fetch(datesUrl);
-        const data = await response.json();
-        setDates(data.records);
-        setIsLoading(false);
-        console.log(data.records)
-    }
-    return (
-        
+  const getFeed = async () => {
+    setIsLoading(true);
+    const response = await fetch(feedUrl);
+    const data = await response.json();
+    setFeed(data.records);
+    setIsLoading(false);
+  };
+  const getDates = async () => {
+    setIsLoading(true);
+    const response = await fetch(datesUrl);
+    const data = await response.json();
+    setDates(data.records);
+    setIsLoading(false);
+    console.log(data.records);
+  };
+  return (
+    <>
+      <h1>Feed</h1>
+      {isLoading ? (
+        <div className="loading">Loading Feed... &nbsp;</div>
+      ) : (
         <>
-            <h1>Feed</h1>
-            {isLoading ? (
-                <div className="loading">Loading Feed... &nbsp;</div>
-            ): (
-                <>
-                 {feed.map(f => (
-                     <ul key={f.id} className="date">
-                        <li>{f.fields.Date}</li>
-                        <li>{f.fields.Headline}</li>
-                        <li>
-                            <a href={f.fields.Link}>read more</a>
-                            
-                        </li>
-                     </ul>
-                 ))}
-                </>
-            )}
-
-            
-            <h1>Events</h1>
-            {isLoading ? (
-                <div className="loading">Loading Dates... &nbsp;</div>
-            ) : (
-                <>
-                 {dates.map((date, index) => (
-                     <ul key={date.id} className="event">
-                         <li className="date">{date.fields.Date}</li>
-                         <li>{date.fields.Name}</li>
-                         <li>{date.fields.Venue}</li>
-                         <li>{date.fields.City}</li>
-                         <li>{date.fields.Country}</li>
-                         <li>{date.fields.MusicBy}</li>
-                         {date.fields.Artwork &&      
-                       <div className="hoverImage">
-                        <img src={date.fields.Artwork[0].thumbnails.large.url}
-                        alt={date.fields.Name} style={{zIndex: `${index}`}}
-                        />
-                        </div>
-                        }
-                        
-                        {date.fields.Link && 
-                        <a href={date.fields.Link}>Go to Event</a>
-                        }
-                     </ul>
-                 ))}
-                </>
-            )}
-            
+          {feed.map(f => (
+            <ul key={f.id} className="date">
+              <li>{f.fields.Date}</li>
+              <li>{f.fields.Headline}</li>
+              <li>
+                <a href={f.fields.Link}>read more</a>
+              </li>
+            </ul>
+          ))}
         </>
-    )
-}
+      )}
 
-export default Dates
+      <h1>Events</h1>
+      {isLoading ? (
+        <div className="loading">Loading Dates... &nbsp;</div>
+      ) : (
+        <>
+          {dates.map((date, index) => (
+            <ul key={date.id} className="event">
+              <li className="date">{date.fields.Date}</li>
+              <li>{date.fields.Name}</li>
+              <li>{date.fields.Venue}</li>
+              <li>{date.fields.City}</li>
+              <li>{date.fields.Country}</li>
+              <li>{date.fields.MusicBy}</li>
+              {date.fields.Artwork && (
+                <div className="hoverImage">
+                  <img
+                    src={date.fields.Artwork[0].thumbnails.large.url}
+                    alt={date.fields.Name}
+                    style={{ zIndex: `${index}` }}
+                  />
+                </div>
+              )}
+
+              {date.fields.Link && <a href={date.fields.Link}>Go to Event</a>}
+            </ul>
+          ))}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Dates;
