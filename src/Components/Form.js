37a6c9202c3be7bb7c4b28a90ae72d0610af49ed
jakobsuckow <jakob@martian.agency>
2019-10-31 11:58:07 +0100

    added cors origin styling

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import useForm from 'react-hook-form';
-
+import React from "react";
+import useForm from "react-hook-form";
 
 export default function Form() {
-    const {register, errors} = useForm();
+  const { register, errors } = useForm();
 
-
-    return (
-        <form data-netlify="true">
-        <label htmlFor="email">Email Address:</label>
-        <input name="email" ref={register({
-            required: true,
-            pattern: /^\S+@\S+$/i
-            })}/>
-        {errors.email && 'Email address wrong.'}
-        <input type="submit" name="submit" />
-        </form>
-       
-    )
+  return (
+    <form data-netlify="true">
+      <label htmlFor="email">Email Address:</label>
+      <input
+        name="email"
+        ref={register({
+          required: true,
+          pattern: /^\S+@\S+$/i
+        })}
+      />
+      {errors.email && "Email address wrong."}
+      <input type="submit" name="submit" />
+    </form>
+  );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Form from '../Components/Form'
-
+import React from "react";
+import Form from "../Components/Form";
 
 const Releases = () => {
-    return (
-        <>  <h1>Projects</h1>
-            <p>Kuratorium is a collection of independent projects...</p>
-            <br />
-            <p>if you would like to receive updates about future Kuratorium activities, send us your email here</p>
-            <br />
-            <Form />
-        </>
-    )
-}
+  return (
+    <>
+      {" "}
+      <h1>Projects</h1>
+      <p>Kuratorium is a collection of independent projects...</p>
+      <br />
+      <p>
+        if you would like to receive updates about future Kuratorium activities,
+        send us your email here
+      </p>
+      <br />
+      <Form />
+    </>
+  );
+};
 
-export default Releases
+export default Releases;
