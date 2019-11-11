adf7876b3a621479e57be91fa94d891bd8940a1a
jakobsuckow <jakob@martian.agency>
2019-11-11 17:18:01 +0100

    now with different projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,11 +6,15 @@ const Releases = () => {
     <>
       {" "}
       <h1>Projects</h1>
-      <p>Kuratorium is a collection of independent projects...</p>
+      <p>
+        Kuratorium is a collection of independent projects. The first project is
+        curated by Lennart Wiehe and called ‘A New Need for Subtlety and Hiss’.
+        Upon its release in early 2019, you can read all about it here.
+      </p>
       <br />
       <p>
-        if you would like to receive updates about future Kuratorium activities,
-        send us your email here
+        If you would like to receive updates about future Kuratorium activities,
+        submit your email address here.
       </p>
       <br />
       <Form />
