5b6c2719106ecfd9e63c34721a0e5e3848134d71
jakobsuckow <jakob@martian.agency>
2019-11-12 19:41:39 +0100

    new attempt

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,7 +4,7 @@ import useForm from "react-hook-form";
 export default function Form() {
   const { register, handleSubmit, errors } = useForm();
   const submit = data => {
-    console.log(data)
+    console.log(JSON.stringify(data.email))
     fetch('/.netlify/functions/api/submit-form', {
       method: 'POST',
       body: JSON.stringify(data.email)
